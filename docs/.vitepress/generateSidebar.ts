f10b2b1809314545175fb1a162fc95f654a8c36b
solvedDev <solveddev@gmail.com>
2022-07-15 09:46:48 +0200

    fix: sidebar nav generation

--- docs/.vitepress/generateSidebar.ts
+++ docs/.vitepress/generateSidebar.ts
@@ -5,101 +5,110 @@ import fs from 'fs'
 import { iterateDir } from './utils/iterateDir'
 
 interface ISidebarItemWithData {
-    text: string
-    link: string
-    data: {
-        [key: string]: any
-    }
+	text: string
+	link: string
+	data: {
+		[key: string]: any
+	}
 }
 interface ISidebarGroup {
-    text?: string
-    items: ISidebarItemWithData[]
-    collapsed?: boolean
-    collapsible?: boolean
+	text?: string
+	items: ISidebarItemWithData[]
+	collapsed?: boolean
+	collapsible?: boolean
 }
 
 export function generateSidebar(directory: string) {
-    const mainCategoryName = capitalize(directory)
-    const sidebar: ISidebarGroup[] = [
-        {
-            text: mainCategoryName,
-            items: [],
-        },
-    ]
-    const mainDirectory = join(process.cwd(), 'docs', directory)
-    const files = iterateDir(mainDirectory).filter((file) => extname(file) === '.md')
+	const mainCategoryName = capitalize(directory)
+	const sidebar: ISidebarGroup[] = [
+		{
+			text: mainCategoryName,
+			items: [],
+		},
+	]
+	const mainDirectory = join(process.cwd(), 'docs', directory)
+	const files = iterateDir(mainDirectory).filter(
+		(file) => extname(file) === '.md'
+	)
 
-    for (let file of files) {
-        file = file.replace(/\\/g, '/')
-        const formattedPath = relative(mainDirectory, file)
-        const formattedFileName = capitalize(basename(formattedPath).replace(/-/g, ' ').replace('.md', ''))
+	for (let file of files) {
+		file = file.replace(/\\/g, '/')
+		const formattedPath = relative(mainDirectory, file)
+		const formattedFileName = capitalize(
+			basename(formattedPath).replace(/-/g, ' ').replace('.md', '')
+		)
 
-        const str = fs.readFileSync(file, 'utf8')
-        let frontMatter: matter.GrayMatterFile<string>
-        try {
-            frontMatter = matter(str)
-        } catch (err) {
-            throw new Error(
-                `File ${file} has invalid frontmatter! ${err.message}`
-            )
-        }
+		const str = fs.readFileSync(file, 'utf8')
+		let frontMatter: matter.GrayMatterFile<string>
+		try {
+			frontMatter = matter(str)
+		} catch (err) {
+			throw new Error(
+				`File ${file} has invalid frontmatter! ${err.message}`
+			)
+		}
 
-        const pathParts = dirname(file).split('/')
-        const parentFolder = pathParts[pathParts.length - 1].replace(/-/g, ' ')
+		const pathParts = dirname(file).split('/')
+		const parentFolder = pathParts[pathParts.length - 1].replace(/-/g, ' ')
 
-        const elementTitle = ((formattedFileName === 'Index' && capitalize(parentFolder) === mainCategoryName) || formattedFileName !== 'Index' ? formattedFileName : capitalize(parentFolder))
-        const element = {
-            text: frontMatter.data.title ?? elementTitle,
-            link: `/${directory}/${formattedPath.replace('.md', '').replace(/\\/g, '/')}.html`,
-            data: frontMatter.data
-        }
+		const elementTitle =
+			(formattedFileName === 'Index' &&
+				capitalize(parentFolder) === mainCategoryName) ||
+			formattedFileName !== 'Index'
+				? formattedFileName
+				: capitalize(parentFolder)
+		const element = {
+			text: frontMatter.data.title ?? elementTitle,
+			link: `/${directory}/${formattedPath
+				.replace('.md', '')
+				.replace(/\\/g, '/')}.html`,
+			data: frontMatter.data,
+		}
 
-        if (frontMatter.data.sidebar) {
-            const categoryName = capitalize(frontMatter.data.sidebar)
-            const existingCategory =  sidebar.find((cat) => cat.text === categoryName)
-            if (existingCategory) {
-                existingCategory.items.push(element)
-            } else {
-                sidebar.push({
-                    text: categoryName,
-                    collapsed: true,
-                    collapsible: true,
-                    items: [element]
-                })
-            }
-        }
-    }
+		if (frontMatter.data.sidebar) {
+			const categoryName = capitalize(frontMatter.data.sidebar)
+			const existingCategory = sidebar.find(
+				(cat) => cat.text === categoryName
+			)
+			if (existingCategory) {
+				existingCategory.items.push(element)
+			} else {
+				sidebar.push({
+					text: categoryName,
+					collapsed: true,
+					collapsible: true,
+					items: [element],
+				})
+			}
+		}
+	}
 
-    for (const category of sidebar) {
-        // Sorting logic borrowed and slightly modified from bedrock wiki :)
-        category.items.sort(
-            ({ data: dataA, text: textA }, { data: dataB, text: textB }) => {
-                // Default to max int, so without nav order you will show second
-                // Multiply by category value if it exists
-                const navA =
-                    (dataA.nav_order || 50) * 100 ||
-                    Number.MAX_SAFE_INTEGER
-                const navB =
-                    (dataB.nav_order || 50) * 100 ||
-                    Number.MAX_SAFE_INTEGER
-    
-                // Tie goes to the text compare! (Will also apply for elements without nav order)
-                if (navA == navB) {
-                    return textA.localeCompare(textB)
-                }
-    
-                // Return nav order
-                return navA - navB
-            }
-        )
-    }
+	for (const category of sidebar) {
+		// Sorting logic borrowed and slightly modified from bedrock wiki :)
+		category.items.sort(
+			({ data: dataA, text: textA }, { data: dataB, text: textB }) => {
+				// Default to max int, so without nav order you will show second
+				// Multiply by category value if it exists
+				const navA = dataA.nav_order ?? 50
+				const navB = dataB.nav_order ?? 50
 
-    return sidebar
+				// Tie goes to the text compare! (Will also apply for elements without nav order)
+				if (navA == navB) {
+					return textA.localeCompare(textB)
+				}
+
+				// Return nav order
+				return navA - navB
+			}
+		)
+	}
+
+	return sidebar
 }
 
 function capitalize(text: string) {
-    return text
-        .split(' ')
-        .map((word) => `${word[0].toUpperCase()}${word.substring(1)}`)
-        .join(' ')
+	return text
+		.split(' ')
+		.map((word) => `${word[0].toUpperCase()}${word.substring(1)}`)
+		.join(' ')
 }
